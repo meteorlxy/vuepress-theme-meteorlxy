69ba45f3412a99191a18c6bbdf553cb116daa479
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-03-15 15:55:50 -0300

    feat($search): Filter on Home with themeConfig
    suport Filter Categories, Tags and Search on Home Layout;
    allow enable this feature on themeConfig homeFilters object;
    default state of this Filters is disabled.

--- lib/layouts/Home.vue
+++ lib/layouts/Home.vue
@@ -1,17 +1,57 @@
 <template>
   <div class="home">
-    <PostsList />
+    <PostsFilter
+      v-if="search || tags || categories"
+      v-model="posts"
+      :posts="$posts"
+      :categories="categories"
+      :tags="tags"
+      :search="search"
+    />
+
+    <PostsList :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostsFilter from '../components/PostsFilter.vue'
 import PostsList from '../components/PostsList.vue'
 
 export default {
   name: 'Home',
 
   components: {
+    PostsFilter,
     PostsList,
   },
+
+  data () {
+    return {
+      posts: null,
+    }
+  },
+
+  created () {
+    this.posts = this.$posts
+  },
+
+  computed: {
+    homeFilters () {
+      return this.$themeConfig.homeFilters || {}
+    },
+
+    categories () {
+      return this.homeFilters.categories || false
+    },
+
+    tags () {
+      return this.homeFilters.tags || false
+    },
+
+    search () {
+      return this.homeFilters.search || false
+    },
+  },
 }
+
 </script>
