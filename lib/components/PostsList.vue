4c79f9eadf21b3f56b3e724de1fcbb95b0482484
meteorlxy <meteor.lxy@foxmail.com>
2019-04-22 06:39:17 -0500

    feat: posts pagination (#17)

--- lib/components/PostsList.vue
+++ lib/components/PostsList.vue
@@ -9,19 +9,35 @@
         {{ $themeConfig.lang.noRelatedPosts }}
       </div>
 
-      <TransitionFadeSlide
+      <div
         v-else
-        tag="div"
-        direction="x"
-        group
+        class="posts-items"
+        :key="page"
       >
-        <PostsListItem
-          v-for="post in listPosts"
-          :key="post.path"
-          :post="post"
-        />
-      </TransitionFadeSlide>
+        <TransitionFadeSlide
+          tag="div"
+          direction="x"
+          group
+        >
+          <PostsListItem
+            v-for="post in pagePosts"
+            :key="post.path"
+            :post="post"
+            :each-side="2"
+          />
+        </TransitionFadeSlide>
+      </div>
     </TransitionFadeSlide>
+
+    <div
+      v-if="total > 1"
+      class="posts-paginator"
+    >
+      <Pagination
+        v-model="page"
+        :total="total"
+      />
+    </div>
   </div>
 </template>
 
@@ -29,6 +45,7 @@
 import compareDesc from 'date-fns/compare_desc'
 import TransitionFadeSlide from './TransitionFadeSlide.vue'
 import PostsListItem from './PostsListItem.vue'
+import Pagination from './Pagination.vue'
 
 export default {
   name: 'PostsList',
@@ -36,6 +53,7 @@ export default {
   components: {
     TransitionFadeSlide,
     PostsListItem,
+    Pagination,
   },
 
   props: {
@@ -46,9 +64,23 @@ export default {
     },
   },
 
+  data () {
+    return {
+      page: 1,
+    }
+  },
+
   computed: {
+    perPage () {
+      return this.$themeConfig.pagination.perPage || 5
+    },
+
+    total () {
+      return Math.ceil(this.listPosts.length / this.perPage)
+    },
+
     listPosts () {
-      const listPosts = this.posts || this.$posts
+      const allPosts = this.posts || this.$posts
 
       const pageSort = (p1, p2) => {
         if (p1.top === p2.top) {
@@ -60,7 +92,19 @@ export default {
         return p2.top ? 1 : -1
       }
 
-      return listPosts.sort(pageSort)
+      return allPosts.sort(pageSort)
+    },
+
+    pagePosts () {
+      const begin = (this.page - 1) * this.perPage
+      const end = begin + this.perPage
+      return this.listPosts.slice(begin, end)
+    },
+  },
+
+  watch: {
+    listPosts () {
+      this.page = 1
     },
   },
 }
